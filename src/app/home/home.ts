992aac68d6e5bf53811f898e7bae82fa88f33a3a
Antlin23 <anton.jumkil@gmail.com>
2025-06-07 20:01:15 +0200

    added created by Anton

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -10,8 +10,8 @@ import { RouterModule } from '@angular/router';
 })
 export class Home {
   featureImages = [
-    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=600&q=80',
     'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=600&q=80',
+    'https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=600&q=80',
     'https://images.unsplash.com/photo-1503676382389-4809596d5290?auto=format&fit=crop&w=600&q=80',
   ];
 
@@ -22,7 +22,7 @@ export class Home {
   ];
 
   featureDescriptions = [
-    'Access tutorials and resources for various programming languages',
+    'Access lessons and resources for various programming languages',
     'Collaborate with other developers in real-time',
     'Engage in coding challenges and competitions',
   ];
